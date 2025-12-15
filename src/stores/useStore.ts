7d700674088418f73ee489e1413aa5dd0bbe1fae
devbekasyl <devbekasyl@gmail.com>
2025-12-16 04:05:54 +0500

    fix: store add persist data

--- src/stores/useStore.ts
+++ src/stores/useStore.ts
@@ -1,30 +1,59 @@
 import { create } from "zustand";
 
+import { CARS_PER_PAGE } from "../constant";
+import { api } from "../services/api";
+
 import type { Car } from "../types";
 
-interface User {
-    id: number;
-    name: string;
-    color: string;
-    winnerName: string;
-    winnerTime: number;
+interface CarState {
+    cars: Car[];
+    totalCars: number | null;
+    page: number;
+    inputs: {
+        name: string;
+        color: string;
+    };
+    selectedCarId: number | null;
+    setPage: (page: number) => void;
+    setInputs: (name: string, color: string) => void;
+    selectCar: (car: Car) => void;
+    fetchCars: () => Promise<void>;
+    createCar: () => Promise<void>;
+    updateCar: () => Promise<void>;
+    deleteCar: (id: number) => Promise<void>;
 }
 
-interface Action {
-    setCar: (car: Car) => void;
-    clearCar: () => void;
-    setWinner: (name: string, time: number) => void;
-}
-
-const useStore = create<User & Action>((set) => ({
-    id: 0,
-    name: "",
-    color: "",
-    winnerName: "",
-    winnerTime: 0,
-    setCar: (car: Car) => set(() => ({ ...car })),
-    clearCar: () => set(() => ({ id: 0, name: "", color: "" })),
-    setWinner: (name: string, time: number) => set(() => ({ winnerName: name, winnerTime: time })),
+const useStore = create<CarState>((set, get) => ({
+    cars: [],
+    totalCars: 0,
+    page: 1,
+    inputs: { name: "", color: "#000000" },
+    selectedCarId: null,
+    setPage: (page) => set({ page }),
+    setInputs: (name, color) => set({ inputs: { name, color } }),
+    selectCar: (car) =>
+        set({ selectedCarId: car.id, inputs: { name: car.name, color: car.color } }),
+    fetchCars: async () => {
+        const { page } = get();
+        const response = await api.cars.getCars({ _page: page, _limit: CARS_PER_PAGE });
+        set({ cars: response.data, totalCars: response.totalCount });
+    },
+    createCar: async () => {
+        const { inputs, fetchCars } = get();
+        await api.cars.createCar({ name: inputs.name, color: inputs.color });
+        await fetchCars();
+    },
+    updateCar: async () => {
+        const { selectedCarId, inputs, fetchCars } = get();
+        if (selectedCarId) {
+            await api.cars.updateCar(selectedCarId, { name: inputs.name, color: inputs.color });
+            await fetchCars();
+        }
+    },
+    deleteCar: async (id) => {
+        await api.cars.deleteCar(id);
+        await get().fetchCars();
+    },
 }));
 
 export default useStore;
